42ac16eb817b92c29d4c84749e6d706553a03800
RedContritio <RedContritio@qq.com>
2021-12-18 10:01:27 +0800

    update bird rotate

--- game.py
+++ game.py
@@ -1,18 +1,25 @@
 from typing import Tuple
+from imgs.bird import DEFAULT_BIRD_HEIGHT
+
+from objects.ground import GROUND_HEIGHT
 
 BIRD_LEFT_POSITION = 0.2
 BIRD_RIGHT_POSITION = 0.5
 BIRD_SPEED = 3
-BIRD_CLICK_SPEED = -4
-WORLD_GRAVITY = 0.3
+BIRD_CLICK_SPEED = -5
+WORLD_GRAVITY = 0.4
+
+GAME_STATE_INIT = 0
+GAME_STATE_RUNNING = 1
+GAME_STATE_END = 2
 
 class Game:
     def __init__(self, window_size: Tuple[int]) -> None:
-        self.bird_speed = [BIRD_SPEED, 0]
+        self.window_size = window_size
         self.bird_xrange = [int(BIRD_LEFT_POSITION * window_size[0]), int(BIRD_RIGHT_POSITION * window_size[0])]
-        self.bird_world_position = [0, window_size[1] // 2]
-        self.camera_rect = [0, 0, window_size[0], window_size[1]]
-        self.fitCamera()
+        self.status = GAME_STATE_INIT
+        self.reset()
+        self.pipes = []
 
     def fitCamera(self):
         if self.bird_world_position[0] < self.camera_rect[0] + self.bird_xrange[0]:
@@ -21,16 +28,41 @@ class Game:
             self.camera_rect[0] = self.bird_world_position[0] - self.bird_xrange[1]
 
     def update(self):
-        for i in range(2):
-            self.bird_world_position[i] += self.bird_speed[i]
-        self.bird_speed[1] += WORLD_GRAVITY
+        if self.status == GAME_STATE_RUNNING:
+            for i in range(2):
+                self.bird_world_position[i] += self.bird_speed[i]
+            self.bird_speed[1] += WORLD_GRAVITY
+            if self.checkCollision():
+                self.status = GAME_STATE_END
 
         self.fitCamera()
     
+    def reset(self):
+        self.bird_speed = [BIRD_SPEED, 0]
+        self.bird_world_position = [0, self.window_size[1] // 2]
+        self.camera_rect = [0, 0, self.window_size[0], self.window_size[1]]
+        self.fitCamera()
+
     @property
     def bird_position(self):
         return (self.bird_world_position[0] - self.camera_rect[0], self.bird_world_position[1] - self.camera_rect[1])
 
+    def checkCollision(self):
+        if self.bird_world_position[1] <= 0 or self.bird_world_position[1] + DEFAULT_BIRD_HEIGHT >= self.window_size[1] - GROUND_HEIGHT:
+            return True
+
     def click(self):
-        self.bird_speed[1] = BIRD_CLICK_SPEED
+        if self.status == GAME_STATE_RUNNING:
+            if self.bird_speed[1] * BIRD_CLICK_SPEED < 0:
+                self.bird_speed[1] = 0
+            self.bird_speed[1] += BIRD_CLICK_SPEED
+        if self.status == GAME_STATE_INIT:
+            self.status = GAME_STATE_RUNNING
+        if self.status == GAME_STATE_END:
+            self.reset()
+            self.status = GAME_STATE_INIT
+
+    @property
+    def running(self):
+        return self.status == GAME_STATE_RUNNING
         
--- objects/bird.py
+++ objects/bird.py
@@ -1,29 +1,40 @@
 from typing import Tuple
 import pygame
+import math
 from pygame.sprite import AbstractGroup
 from game import Game
 
 from imgs.bird import DEFAULT_BIRD_SIZE, get_birds
 
-BIRD_FRAME_LENGTH = 3
+BIRD_FRAME_LENGTH = 2
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self, position: Tuple[int], size: Tuple[int] = DEFAULT_BIRD_SIZE) -> None:
         super().__init__()
         self.position = position
+        self.speed = []
         self.size = size
         bird_images = get_birds()
         print(bird_images[0])
         
         self.images = [pygame.image.frombuffer(bird.tobytes(), bird.shape[:2][::-1], 'RGBA') for bird in bird_images]
         self.images.append(self.images[1])
+        self.angle = 0
         self.frame = 0
         
     def update(self, game: Game) -> None:
-        self.frame += 1
         self.position = game.bird_position
+        self.speed = game.bird_speed
+        self.angle = math.atan2(self.speed[1], self.speed[0] * 2) # less rotate angle
+        if game.running:
+            if self.speed[1] < 0:
+                self.frame += 1
+            else:
+                self.frame = 0
 
     @property
     def image(self):
         i = (self.frame // BIRD_FRAME_LENGTH) % len(self.images)
-        return self.images[i]
+        source = self.images[1] if self.speed[1] >= 0 else self.images[i]
+        dst = pygame.transform.rotate(source, -self.angle / math.pi * 180)
+        return dst
